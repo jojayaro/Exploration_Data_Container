698a6048420704cfc1681bef277a46e82b5b8802
Jesus Jayaro <jayaro@gmail.com>
2021-01-24 23:16:03 -0700

    Update to App

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
+import pydeck as pdk
 
+#st.set_page_config(layout="wide")
 '''
 # Exploration Data App
 This is a Streamlit App deployed on a local K3s cluster that presents data from Alberta Energy Regulator (AER)
@@ -14,12 +16,16 @@ Server currently runs a script daily to download the text files, parses them int
 to the repository, which triggers a container update to rebuild the **latest** image, which then is pulled by the K3s cluster
 '''
 
-#Open ST1 data
-ST1 = pd.read_csv('ST1.csv')
+#Open data
 ST49 = pd.read_csv('ST49.csv')
+ST1_2020 = pd.read_csv('ST1-2020.csv')
+ST1_2020.rename(columns={'Lat': 'lat', 'Long': 'lon'}, inplace=True)
+ST1_2021 = pd.read_csv('ST1-2021.csv')
+ST1_2021.rename(columns={'Lat': 'lat', 'Long': 'lon'}, inplace=True)
+ST1 = pd.concat([ST1_2020, ST1_2021])
 
 '''
-## Well Licences Issued by Week (2020 vs 2021)
+## Well Licences Issued per Week (2020 vs 2021)
 Licences issued for all operators and all substances
 '''
 week_year = ST1
@@ -30,75 +36,74 @@ week_year.columns = week_year.columns.droplevel(0)
 week_year = week_year.rename_axis(None, axis=1)
 week_year = week_year.reset_index().drop('index', axis=1, errors='ignore')
 week_year.fillna(0, inplace=True)
+week_year.drop(week_year.tail(1).index,inplace=True)
 
-chart=[]
 fig1 = go.Figure(data=[
     go.Bar(name='2020', x=week_year['WEEK'], y=week_year[2020]),
     go.Bar(name='2021', x=week_year['WEEK'], y=week_year[2021])
 ])
-# Change the bar mode
 fig1.update_layout(barmode='group')
 
 st.plotly_chart(fig1)
 
 '''
-## Weekly Activity (2020)
+## Well Licences by Substance per Week (2021)
 
-Licenses issued by week broken down by substance (for all operators)
 '''
-df = ST1[ST1['YEAR'] == 2020]
+week_subs = ST1_2021
 
-#Data Cleaning for Weekly Data
-df = pd.pivot_table(df, index=['WEEK'], columns=['SUBSTANCE'], values=['LICENSEE'], aggfunc='count')
-df.columns = df.columns.droplevel(0)
-df = df.rename_axis(None, axis=1)
-if isinstance(df, (pd.DatetimeIndex, pd.MultiIndex)):
-	df = df.to_frame(index=False)
+week_subs = pd.pivot_table(week_subs, index=['WEEK'], columns=['SUBSTANCE'], values=['LICENSEE'], aggfunc='count')
+week_subs.columns = week_subs.columns.droplevel(0)
+week_subs = week_subs.rename_axis(None, axis=1)
+week_subs = week_subs.reset_index().drop('index', axis=1, errors='ignore')
+week_subs.fillna(0, inplace=True)
+week_subs.drop(week_subs.tail(1).index,inplace=True)
 
-#Remove any pre-existing indices for ease of use in the D-Tale code, but this is not required
-df = df.reset_index().drop('index', axis=1, errors='ignore')
-df.columns = [str(c) for c in df.columns]  # update columns to strings in case they are numbers
+fig = go.Figure(data=[
+    go.Bar(name='CRUDE BITUMEN', x=week_subs['WEEK'], y=week_subs['CRUDE BITUMEN']),
+    go.Bar(name='CRUDE OIL', x=week_subs['WEEK'], y=week_subs['CRUDE OIL']),
+    go.Bar(name='GAS', x=week_subs['WEEK'], y=week_subs['GAS'])
+])
 
-chart_data = pd.concat([
-	df['WEEK'],
-	df['CRUDE OIL'],
-	df['CRUDE BITUMEN'],
-	df['GAS'],
-], axis=1)
-chart_data = chart_data.sort_values(['WEEK'])
-chart_data = chart_data.rename(columns={'WEEK': 'x'})
-chart_data = chart_data.dropna()
-
-#Plotly Graph
-
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=chart_data['x'], y=chart_data['CRUDE OIL'],
-                    mode='lines',
-                    name='Crude Oil'))
-fig.add_trace(go.Scatter(x=chart_data['x'], y=chart_data['GAS'],
-                    mode='lines',
-                    name='Gas'))
-fig.add_trace(go.Scatter(x=chart_data['x'], y=chart_data['CRUDE BITUMEN'],
-                    mode='lines', name='Crude Bitumen'))
+fig.update_layout(barmode='group')
 
 st.plotly_chart(fig)
 
-
 '''
-## AER ST1 Well Licences Locations by Operator
+## Well Licences Locations by Operator
 
-Note: Zoom out to see all locations for the selected operator
 '''
 #Create Dropdown by Licensee
-licensee = ST1.LICENSEE.unique()
+licensee = list(ST1_2021.LICENSEE.unique())
 option = st.selectbox('Select Operator from the list below to update locations', sorted(licensee))
-filter_data = ST1[ST1['LICENSEE'] == option]
+filter_data = ST1_2021[ST1_2021['LICENSEE'] == option]
 
 #Map from selected Licensee
-st.map(filter_data)
+#st.map(filter_data)
+midpoint = (np.average(ST1_2021['lat']), np.average(ST1_2021['lon']))
+
+st.pydeck_chart(pdk.Deck(
+     map_style='mapbox://styles/mapbox/dark-v9',
+     initial_view_state=pdk.ViewState(
+         latitude=midpoint[0],
+         longitude=midpoint[1],
+         zoom=4,                  
+     ),
+     layers=[         
+         pdk.Layer(
+             'ScatterplotLayer',
+             data=filter_data,
+             get_position='[lon, lat]',
+             get_color='[200, 30, 0, 160]',
+             get_radius=10000,
+         ),
+     ],
+))
+
 '''
 ## Data for selected Operator
 
 '''
 #Dataframe showing filtered data from selected Licensee
 st.dataframe(filter_data.sort_values(by='WEEK'))
+
