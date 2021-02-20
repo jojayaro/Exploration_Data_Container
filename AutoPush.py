0d0b736e833cba11dc05e4340bd71c65f65d3fa2
Jesus Jayaro <jayaro@gmail.com>
2021-02-20 10:32:50 -0700

    Update CSV with Daily Data

--- AutoPush.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-import subprocess as cmd
-
-cp = cmd.run("git -C /home/sidefxs/Documents/Exploration_Data_Container/ add .", check=True, shell=True)
-cp = cmd.run('git -C /home/sidefxs/Documents/Exploration_Data_Container/ commit -m "Update CSV with Daily Data"', check=True, shell=True)
-cp = cmd.run("git -C /home/sidefxs/Documents/Exploration_Data_Container/ push -u origin master -f", check=True, shell=True)
